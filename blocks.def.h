83500135bd733b3fb1b500eab02d0e02d30d2fda
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-02-20 15:36:32 -0300

    Dividida barra em dois

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/    /*Command*/                                                          /*Update Interval*/    /*Update Signal*/
-	{"",          "date '+%a %d/%m/%Y - %H:%M;'",                                      60,                    0},
-	{" ",      "tocando musica",                                                     5,                     0},
-	{" ",         "",                                                                  0,                     0},
-	{" ",       "amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }'",  0,                     9},
+	{"",          "netspeed",                                                          2,                     0},
 	{" ",         "",                                                                  0,                     0},
 	{"",          "pacupdates",                                                        0,                     0},
+	{"^",         "",                                                                  0,                     0},
+	{"",          "date '+%a %d/%m/%Y - %H:%M'",                                       60,                    0},
+	{";",         "",                                                                  0,                     0},
+	{" ",      "tocando musica",                                                    5,                     0},
+	{" ",         "",                                                                  0,                     0},
+	{"",          "volume",                                                            0,                     9},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
