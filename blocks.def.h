26ebd43bdfd74cc90478b4a177599227fd2d1871
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-02-20 00:45:26 -0300

    Data no meio da barra

--- blocks.def.h
+++ blocks.def.h
@@ -1,13 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/     /*Command*/                                                           /*Update Interval*/    /*Update Signal*/
-	{" ",        "tocando musica",                                                     5,                     0},
+	{"",           "date '+%a %d/%m/%Y - %H:%M;'",                                       60,                    0},
 
-	{" ",        "amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }'",    0,                     9},
+	{" ",        "tocando musica",                                                    5,                     0},
 
-	{"",           "date '+%a %d/%m/%Y - %H:%M │'",                                       60,                    0},
+	{"",           "echo ' │ '",                                                         0,                     0},
+
+	{" ",        "amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }'",   0,                     9},
+
+	{"",           "echo ' │'",                                                          0,                     0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " │ ";
+static char delim[] = "";
 static unsigned int delimLen = 5;
