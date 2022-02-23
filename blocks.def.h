eeec27398ee2928c4531f287b47072d659827d2f
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-02-23 14:32:10 -0300

    Removido netspeed e disk.

--- blocks.def.h
+++ blocks.def.h
@@ -1,15 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/    /*Command*/                                                          /*Update Interval*/    /*Update Signal*/
-	{"",          "netspeed",                                                          2,                     0},
-	{"",          "",                                                                  0,                     0},
-	{"",          "disk",                                                              0,                     0},
-	{"^",         "",                                                                  0,                     0},
-	{"",          "date '+%a %d/%m/%Y - %H:%M'",                                       60,                    0},
-	{";",         "",                                                                  0,                     0},
-	{" ",      "tocando musica",                                                     5,                     0},
-	{" ",         "",                                                                  0,                     0},
+	{"",          "musica estado",                                                     5,                     11},
 	{"",          "volume",                                                            0,                     9},
+	{";",         "",                                                                  0,                     0},
+	{"",          "date '+%a %d/%m/%Y - %H:%M'",                                       60,                    0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
